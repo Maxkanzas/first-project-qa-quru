54e40a586befd575337ef71c12499e9eba53977b
Kantselyaristov Maxim <maxkanz1991@gmail.com>
2025-08-20 16:27:13 +0400

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,7 +1,7 @@
 package guru.qa;
 
 class Main {
-    public static void main(String... args) {
+    public static void main(String[] args) {
         String hello = "Hello Maxim";
         System.out.printf(hello);
     }
